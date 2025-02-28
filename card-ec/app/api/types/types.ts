4f51aa9f32cda2663a1f97fa254a10c92e2791bb
世古侑大 <yseko0825@gmail.com>
2025-02-28 14:12:34 +0900

    Cardsテーブルのカラムの型を定義しました

---
+++ card-ec/app/api/types/types.ts
@@ -0,0 +1,10 @@
+type Card = {
+    cardId: string,
+    cardName: string,
+    rarity: string,
+    cardImage: string,
+    Purchase_price: string,
+    Sales_price: string,
+    created_at: Date,
+    updated_at: Date
+}
