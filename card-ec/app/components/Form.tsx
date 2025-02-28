4e16d4cb324b1f0fa35c80755aca8f0b1a676c87
世古侑大 <yseko0825@gmail.com>
2025-02-28 14:05:57 +0900

    検索内容をサーバーに送り、その値が含まれる情報を取得する処理を追加しました

--- card-ec/app/components/Form.tsx
+++ card-ec/app/components/Form.tsx
@@ -1,9 +1,37 @@
-import "../../styles/form.css"
+'use client'
 
-export default function Form() {
+import "../../styles/form.css"
+import React, { useState } from "react"
+
+export const Form = ( {onServerResponse}:  { onServerResponse: (cardsData: Card[]) => void }) => {
+    const [value, setValue] = useState("");
+
+    const onSubmitHandler: React.FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/searchCard`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                value: value
+            }),
+        });
+
+        if(res.ok) {
+            const cardsData = await res.json();
+            onServerResponse(cardsData);
+        } else {
+            const cardsData = await res.json();
+            alert(cardsData)
+        }
+    }
+
+    const changeHandler = (e: string) => {
+        setValue(e)
+    }
+
     return (
-        <form action="/">
-              <input type="text" placeholder="カード検索" className="name-input"/>
+        <form action="/" onSubmit={(e) => onSubmitHandler(e)}>
+              <input type="text" placeholder="カード検索" className="name-input" value={value} onChange={(e) => changeHandler(e.target.value)}/>
               <input type="submit" value={"検索"} className="submit-btn"/>
         </form>
     )
