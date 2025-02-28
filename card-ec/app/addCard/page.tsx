ea0eddd9b794332668b2123f3e3b2a5d6b08d2b5
世古侑大 <yseko0825@gmail.com>
2025-02-28 14:17:02 +0900

    サーバーにリクエストを送り、カード情報を受け取る処理を追加しました。

--- card-ec/app/addCard/page.tsx
+++ card-ec/app/addCard/page.tsx
@@ -1,9 +1,36 @@
+'use client'
+
 import Image from "next/image"
 import Link from "next/link"
-import Form from "../components/Form"
+import { Form } from "../components/Form"
 import "../../styles/addCard.css"
+import { useEffect, useState } from "react"
 
 export default function AddCardPage() {
+
+    let cardsData: Card[] = [];
+
+    const [ cardData, setCardData] = useState<Card[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getCard`);
+
+            if(res.ok) {
+                const data = await res.json();
+                setCardData(data);
+            } else {
+                const errorData = await res.json();
+                alert(errorData);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    const handleServerResponse = (data: Card[]) => {
+        setCardData(data)
+    }
     return (
         <main>
             <Link href={"/"} className="back">＜ トップページに戻る</Link>
@@ -11,322 +38,41 @@ export default function AddCardPage() {
                 <h2>
                     コレクションに追加したいカードを登録
                 </h2>
-                <Form />
+                <Form onServerResponse={(data: Card[]) => handleServerResponse(data)}/>
             </div>
             <ul className="card-list">
-                <li className="card-item">
-                    <Image
+                {cardData.map((card) => (
+                    <li className="card-item" key={card.cardId}>
+                    <div className="image-area">
+                        <Image
                         className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
+                        src={card.cardImage}
+                        alt={card.cardName + card.rarity + card.cardId}
                         width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
+                        height={140}
+                        />
+                    </div>
+                    <p>{`${card.cardName} 【${card.rarity}】`}</p>
+                    <div className="price-area">
+                        <p className="sale">{`販売${card.Sales_price}`}</p>
+                        <p className="purchase">{`買取${card.Purchase_price}`}</p>
+                    </div>
                     <button className="add-btn">
                         登録する
                     </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p>ニンフィアex【sar】</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
-                <li className="card-item">
-                    <Image
-                        className="card-image"
-                        src={"/products/IMG_7095.jpeg"}
-                        alt={"ニンフィアex"}
-                        width={100}
-                        height={80}
-                    />
-                    <p className="name">ニンフィアex</p>
-                    <button className="add-btn">
-                        登録する
-                    </button>
-                </li>
+                    </li>
+                ))}
             </ul>
+            <div className="pagination">
+              <button>前へ</button>
+              <div>
+                <button className="num-btn">1</button>
+                <button className="num-btn">2</button>
+                <button className="num-btn">3</button>
+                <button className="num-btn">4</button>
+              </div>
+              <button>次へ</button>
+            </div>
         </main>
     )
 }
