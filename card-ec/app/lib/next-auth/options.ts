a04f975439c90e3ae252b0676df26c1bd4dda106
世古侑大 <yseko0825@gmail.com>
2025-03-12 17:51:17 +0900

    メールアドレス設定していた部分をユーザー名に修正。callbacksの部分にトークンにユーザー情報を追加する処理及びトークンのユーザー情報をセッションに追加する処理を追加。

--- card-ec/app/lib/next-auth/options.ts
+++ card-ec/app/lib/next-auth/options.ts
@@ -1,6 +1,5 @@
 import Credentials from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
-import { User } from "next-auth";
 import prisma from "../prisma";
 import bcrypt from 'bcrypt'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
@@ -9,55 +8,73 @@ export const nextAuthOptions: NextAuthOptions = {
     debug: false,
     providers: [
         Credentials({
-            id: 'user',
-            name: 'User',
+            id: 'credentials',
+            name: 'Credentials',
             credentials: {
-                email: { label: 'メールアドレス', type: 'email', placeholder: 'メールアドレス'},
+                userName: { label: 'ユーザー名', type: 'text', placeholder: 'ユーザー名'},
                 password: { label: 'パスワード', type: 'password'}
             },
             async authorize(credentials) {
-                //メールアドレスまたはパスワードが空の場合
-                if(!credentials?.email || !credentials.password) {
-                    throw new Error('Email and password required');
+
+                console.log(credentials);
+                //ユーザー名またはパスワードが空の場合
+                if(!credentials?.userName || !credentials.password) {
+                    throw new Error('ユーザー名とパスワードは必須入力です。');
                 }
 
-                //送られてきたメールアドレスと一致するメールアドレスがDBに存在するか
-                const user = await prisma.user.findUnique({
-                    where: { email: credentials.email },
+                //送られてきたユーザー名と一致するユーザー名がDBに存在するか
+                const user = await prisma.user.findFirst({
+                    where: { name: credentials.userName },
                 })
 
-                if(!user || !user.password) {
+                if(!user) {
                     throw new Error('Email does not exists')
                 }
 
                 //ハッシュ化されたパスワードと送られてきたパスワードを比較する
                 const isCorrectPassword = await bcrypt.compare(
                     credentials.password,//送られてきたパスワード
-                    user.password,//DBに保存されているハッシュ化されたパスワード
+                    user.password!,//DBに保存されているハッシュ化されたパスワード
                 )
 
                 if(!isCorrectPassword) {
                     throw new Error('Incorrect password')
                 }
 
+                console.log(user);
                 return user
             },
         }),
     ],
     pages: {
-        signIn: '/',
+        signIn: '/login',
+    },
+    //セッション情報をサーバーに保存せず、トークンを生成し、それをクライアント側に保存する設定
+    session: {
+        strategy: 'jwt'
     },
     adapter: PrismaAdapter(prisma),
     callbacks: {
-        session: ({session, user}) => {
-            return {
-                ...session,
-                user: {
-                    ...session.user,
-                    id: user.id
-                }
+        //ログイン時にトークンにユーザー情報を追加する
+        jwt: ({ token, user }) => {
+            if (user) {
+              return {
+                ...token,
+                id: user.id,
+              };
             }
-        }
+            return token;
+        },
+        //トークンからセッション情報を取得
+        session: ({ session, token }) => {
+            return {
+              ...session,
+              user: {
+                ...session.user,
+                id: token.id,
+              },
+            };
+          },
     },
     secret: process.env.AUTH_SECRET,
 }
