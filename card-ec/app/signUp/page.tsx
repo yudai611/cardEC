500efca9c09f36a7fb5c96920d0b310aa7b3b190
世古侑大 <yseko0825@gmail.com>
2025-03-12 17:56:47 +0900

    サインアップ機能を実装しました。

--- card-ec/app/signUp/page.tsx
+++ card-ec/app/signUp/page.tsx
@@ -1,92 +1,60 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import * as z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm, SubmitHandler } from 'react-hook-form'
 import { useState } from "react"
 import "../../styles/signUp.css"
 import Link from 'next/link'
-import prisma from '../lib/prisma'
-import bcrypt from 'bcryptjs'
-type Schema = z.infer<typeof schema>
-
-//入力データの検証ルールを定義
-const schema = z.object({
-    name: z.string().min(2, { message: '2文字以上入力する必要があります。'}),
-    email: z.string().email({ message: 'メールアドレスの方式ではありません。'}),
-    password: z.string().min(6, { message: '6文字以上で入力する必要があります。'})
-})
 
 const SignUpPage = () => {
-    const router = useRouter();
-    const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState('');
+    const [ userName, setUserName] = useState('');//入力されているユーザー名を管理
+    const [ password, setPassword ] = useState('');//入力されているパスワードを管理
+    const [ error, setError ] = useState('');//新規登録失敗時のエラーメッセージを管理
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm({
-        defaultValues: { name: '', email: '', password: ''},
-        resolver: zodResolver(schema)
-    });
+    const handleSubmit = async (e:any) => {
+        e.preventDefault();
 
-    //送信
-    const onSubmit: SubmitHandler<Schema> = async (data) => {
-        setLoading(true);
-        try {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signUp`,
-                {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json'},
-                    body: JSON.stringify({
-                        email: data.email,
-                        password: data.password,
-                        name: data.name
-                    })
-                }
-            );
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signUp`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                userName: userName,
+                password: password
+            }),
+        });
 
-            if(res.ok) {
-                alert('会員登録に成功しました。');
-            } else {
-                alert('会員登録に失敗しました。');
-            }
-        } catch(err) {
-            setMessage('エラーが発生しました。' + err)
-        } finally {
-            setLoading(false);
+        if(res.ok) {
+            alert('新規登録に成功しました。');
+        } else {
+            const data = await res.json();
+            setError(data);
         }
+    }
+
+    //ユーザー名を更新
+    const onChangeUserName = (value: string) => {
+        setUserName(value);
+    }
+    
+    //パスワードを更新
+    const onChangePassword = (value: string) => {
+        setPassword(value);
     }
 
     return (
         <>
             <h2>新規登録</h2>
-            <form onSubmit={handleSubmit(onSubmit)} className="form">
+            <form onSubmit={(e) => handleSubmit(e)} className="form">
                 <label>
-                    メールアドレス<br />
+                    ユーザー名<br />
                     <input 
                         type="text"
                         id="name"
                         className="input"
-                        placeholder='ニックネーム'
-                        {...register('name', { required: true })}
+                        placeholder='ユーザー名'
+                        value={userName}
+                        onChange={(e) => onChangeUserName(e.target.value)}
                     />
-                    <div>{errors.name?.message}</div>
                 </label>
                 <br />
-                <label>
-                    メールアドレス<br />
-                    <input 
-                        type="email"
-                        id="email"
-                        className="input"
-                        placeholder='メールアドレス'
-                        {...register('email', { required: true })}
-                    />
-                    <div>{errors.email?.message}</div>
-                </label>
                 <br />
                 <label>
                     パスワード<br />
@@ -95,15 +63,13 @@ const SignUpPage = () => {
                         id="password"
                         className="input"
                         placeholder='パスワード'
-                        {...register('password', { required: true })}
+                        value={password}
+                        onChange={(e) => onChangePassword(e.target.value)}
                      />
-                    <div>{errors.password?.message}</div>
                 </label>
                 <br />
                 <input type="submit" className="submit-btn" value="新規登録"/>
             </form>
-
-            {message && <div>{message}</div>}
 
             <Link href={"/login"}>ログインはこちら</Link>
         </>
