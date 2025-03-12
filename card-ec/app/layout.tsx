52e60b3558feebc73796b2b87f0df1324986d58a
世古侑大 <yseko0825@gmail.com>
2025-03-12 16:26:35 +0900

    クライアントでセッションを管理するために必要なプロバイダーを定義しているコンポーネントをインポートしbodyの中身をラップしました。

--- card-ec/app/layout.tsx
+++ card-ec/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import { NextAuthProvider } from "./lib/next-auth/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          {children}
+        <NextAuthProvider>
+        {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
