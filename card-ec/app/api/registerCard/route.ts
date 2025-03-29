c412a2c15e75c86dd72369c1f184f1c3ef234a8c
世古侑大 <yseko0825@gmail.com>
2025-03-29 20:04:30 +0900

    登録するカードのID・枚数をリクエストから取得しDBに登録する

--- card-ec/app/api/registerCard/route.ts
+++ card-ec/app/api/registerCard/route.ts
@@ -3,14 +3,20 @@ import { NextResponse } from "next/server";
 
 
 export const POST = async (req: Request, res: Response) => {
-    const { userId, cardId }  = await req.json();
+    const { userId, cardId, count } = await req.json();
 
-    //ユーザが登録したカードを格納するテーブル(User_Card)にレコードを作成
-    // await prisma.user_Card.create({
-    //     data: {
-    //         userId: userId,
-    //         cardId: cardId,
-            
-    //     }
-    // });
+    try {
+        //ユーザが登録したカードを格納するテーブル(User_Card)にレコードを作成
+        await prisma.user_Card.create({
+            data: {
+                userId: userId,
+                cardId: cardId,
+                count: count,
+            }
+        });
+
+        return NextResponse.json({});
+    } catch {
+        return NextResponse.json({});
+    }
 }
